7a29f03e270d345bf0c413504642ca1ecd23cde3
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-04-30 09:54:48 +0300

    Fix flag

--- winreset.ps1
+++ winreset.ps1
@@ -1,3 +1,3 @@
-winget install --accept-package-agreement xbox
+winget install --accept-source-agreements xbox
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
