217bf9b29257f94dabf856d5d49feba25729bcef
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-04-30 09:56:15 +0300

    Add steam

--- winreset.ps1
+++ winreset.ps1
@@ -1,3 +1,4 @@
 winget install --accept-source-agreements xbox
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install steam
