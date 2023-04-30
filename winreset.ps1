58f19636d68a1391cb198c6597b37297a32dcd55
Aleksandr Gorokhov <agorokhov94@gmail.com>
2023-04-30 10:07:57 +0300

    One by one

--- winreset.ps1
+++ winreset.ps1
@@ -1,4 +1,5 @@
-winget install -q 'Xbox' 'Epic Games Launcher' --accept-source-agreements
+winget install 'Epic Games Launcher' --accept-source-agreements
+winget install 'Xbox' --accept-source-agreements
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install steam
