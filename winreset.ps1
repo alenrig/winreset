abea0ec7728a9efc0b44ca8c8541f9ea1dd7f2dc
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-07-04 22:19:22 +0300

    Add sudo package

--- winreset.ps1
+++ winreset.ps1
@@ -1,4 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install sudo
 choco install discord
 choco install --ignore-checksums steam
 choco install --ignore-checksums geforce-experience
