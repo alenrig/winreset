8a1e85eb4493edb00c5e8e6ce59a1e99a884eb42
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-07-05 14:04:29 +0300

    Add agreements

--- winreset.ps1
+++ winreset.ps1
@@ -1,9 +1,9 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install sudo
-choco install discord
-choco install --ignore-checksums steam
-choco install --ignore-checksums geforce-experience
+choco install -y sudo
+choco install -y discord
+choco install -y --ignore-checksums steam
+choco install -y --ignore-checksums geforce-experience
 
-winget install microsoft.pcmanager --accept-source-agreements
-winget install telegram.telegramdesktop --accept-source-agreements
-winget install xbox --accept-source-agreements
+winget install microsoft.pcmanager --accept-source-agreements --accept-package-agreements
+winget install telegram.telegramdesktop --accept-source-agreements --accept-package-agreements
+winget install xbox --accept-source-agreements --accept-package-agreements
