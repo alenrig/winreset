d878556f90ace3bb0852e01b85682ec394d1c119
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-07-05 14:02:26 +0300

    update script

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ A project for fast Windows system reinstallation.
 
 ## Usage
 
-1. `winget install -e --id Git.Git`
-2. Clone this repo
-3. Run winreset.ps1
+1. `Set-ExecutionPolicy Bypass -Scope Process -Force`
+2. `winget install -e --id Git.Git`
+3. Clone this repo
+4. Run winreset.ps1
--- winreset.ps1
+++ winreset.ps1
@@ -1,9 +1,9 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install sudo
 choco install discord
 choco install --ignore-checksums steam
 choco install --ignore-checksums geforce-experience
 
 winget install microsoft.pcmanager --accept-source-agreements
-winget install telegram --accept-source-agreements
+winget install telegram.telegramdesktop --accept-source-agreements
 winget install xbox --accept-source-agreements
